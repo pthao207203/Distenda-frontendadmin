f76b45bd86ded1a060b3dd01e8088257b9d8044d
pthao207203 <149165346+pthao207203@users.noreply.github.com>
2025-04-30 00:19:59 +0700

    fix deploy

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -20,16 +20,13 @@ export const loginController = async (data, setSuccess, setError) => {
 
 
 // [POST] /auth/loginConfirm
-export const loginConfirmController = async (data, setSuccess, setError, navigate) => {
+export const loginConfirmController = async (data, setSuccess, setError) => {
   try {
     const result = await loginConfirmService(data); // Gọi service để xử lý API
     if (result.code === 400) {
       setError(result.message);
     } else {
       setSuccess(result.message || 'Đăng nhập thành công!');
-      setTimeout(() => {
-        navigate('/'); // Điều hướng tới trang chủ
-      }, 3000);
     }
     return result;
   } catch (err) {
